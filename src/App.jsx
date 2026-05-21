d301ad939917946f1de3a9ba584ba6c7d41ecf0c
Jacob Szczudlik <jacobszc@buffalo.edu>
2026-05-21 19:19:20 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+   <div className = "header-div">
+
+    
+    <img className = "header-img-1 "src = "images/pink-flower-bird-hd.png"></img>
+    
+    
+
+     <img className = "header-img-2 "src = "images/rainbow-bird-hd.png"></img>
+    
+
+
+   </div>
+  
+  )
+}
+
+export default App
