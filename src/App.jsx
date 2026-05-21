c2d4e0f504f0db231f8c3b86fe4f8fb19f7ddefd
Jacob Szczudlik <jacobszc@buffalo.edu>
2026-05-21 19:40:37 -0400

    added header stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 
 function App() {
  
 
   return (
-   <div className = "header-div">
+   <div className = "place-holder-container">
+   
+   
+   <div className = "header-div" >
 
     
     <img className = "header-img-1 "src = "images/pink-flower-bird-hd.png"></img>
-    
-    
+     <div className ="title-container">
+    <h1 style ={{justifySelf: "center"}}>Angies Bird Thing</h1>
+    <h2>Mabey Licenced????</h2>
+
+    </div>
 
      <img className = "header-img-2 "src = "images/rainbow-bird-hd.png"></img>
+
+     
     
 
 
    </div>
+
+   <div className = "description-container-top" style ={{width: "100%", height: "100px", backgroundColor: "greenyellow"}}>
+
+   </div>
+   
+   
+   
+   </div> // end of placeholder container
+
+   
   
   )
 }
