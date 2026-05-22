2c7e7be65dfc7cbf23d49e6a87db46689094dc6a
Jacob Szczudlik <jacobszc@buffalo.edu>
2026-05-22 09:56:15 -0400

    added drag and drop hander for images

--- src/ImagePostingComp.jsx
+++ src/ImagePostingComp.jsx
@@ -1,10 +1,55 @@
+import {useState, useEffect} from "react";
+
 function ImagePostingComp(){
 
+    const [images, setImages] = useState([]);
 
+    function dragOverHandler () {
+
+
+        event.preventDefault();
+    }
+
+
+    function dropHandler(event){
+
+    
+    const file = event.dataTransfer.files[0];
+
+    const formData = new FormData();
+
+    formData.append("file", file);
+    
+    setImages(prev => [...prev, file])
+    
+    
+    
+    
+    
+ 
+
+    
+}
+
+
+
+
+
+    useEffect(()=> {
+
+        images.map((image,index) => {
+
+            console.log(image)
+        })
+
+
+    },[images])
+
+   
 
     return (
 
-     <div className = "image-posting-container">
+     <div className = "image-posting-container" onDrop = {dropHandler} onDragOver={dragOverHandler}>
 
         
      </div>
