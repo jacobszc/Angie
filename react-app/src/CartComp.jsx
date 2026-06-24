467cb5829d1221baec46fc797235738938f8a8df
Jacob Szczudlik <jacobszc@buffalo.edu>
2026-06-23 21:01:05 -0400

    added cart and map to disply items in cart, need to pass prop next to cart

--- react-app/src/CartComp.jsx
+++ react-app/src/CartComp.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react"
 import "./styles/CartComp.css"
 
-function CartComp() {
+function CartComp({cart}) {
+    
 
+    const [sampleCart, setSampleCart] = useState([{price: "200", name: "bob", id: "1"}, {price: "300", name: "bill" , id: "2"}, {price: "100", name: "charlie" , id: "3"}])
 
 
     return (
         <div className = "cart-container">
+        <div className="items-container">
 
+            {sampleCart.map((item , index) => (
+            
+             <div className ="item" key = {index}> 
+                <div className = "item-description-container">name: {item.name}</div>
+                <div className = "item-description-container">price: {item.price}</div>
+                <div className = "item-description-container">id: {item.id}</div>
+             </div>
+
+    ))}
+
+
+
+
+        </div>
+     
 
         </div>
     )
